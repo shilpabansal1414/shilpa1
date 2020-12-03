3fe307b0f0bfcfbcfc56443178ceaad756ccf3a5
lalit <lalit@10.0.0.102>
2020-12-03 00:19:29 -0600

    first time pushing

---
+++ codesharingnew/src/pkg/classcode.java
@@ -0,0 +1,13 @@
+package pkg;
+
+public class classcode 
+{
+public void display()
+{
+	System.out.println("main");
+}
+public static void main(String[] args) {
+	classcode cc= new classcode();
+	cc.display();
+}
+}
